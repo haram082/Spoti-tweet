4f68e039e94ebe5feb664de9480d2fadd1e35e0c
Haram Yoon <haramkim082@gmail.com>
2023-08-03 13:19:35 -0700

    config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,8 @@ const config = {
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
